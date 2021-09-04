e21fe882b6675b35b7be716dc26b67f5061857b8
Redlong <changyixie0824@gmail.com>
2021-09-04 10:23:40 +0800

    Add files via upload

---
+++ include/netproc.h
@@ -0,0 +1,12 @@
+#ifndef _NETPROC_H_
+#define _NETPROC_H_
+
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <mcheck.h>
+#include <malloc.h>
+
+#endif
